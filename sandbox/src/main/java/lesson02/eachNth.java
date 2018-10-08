a1a463bb11d1906e37d3ebd83c64762060efaf24
Julik <tarabaroff@gmail.com>
2018-10-08 23:00:44 +0300

    First Task

--- sandbox/src/main/java/lesson02/eachNth.java
+++ sandbox/src/main/java/lesson02/eachNth.java
@@ -5,21 +5,22 @@ public class eachNth {
     public static void main(String[] args) {
 
         everyNth("Julia", 2);
- //     everyNth("mouse", 2);
-  //    everyNth("Achilles", 2);
-  //    everyNth("Boris", 1);
+        everyNth("mouse", 3);
+        everyNth("Achilles", 2);
+        everyNth("Boris", 1);
         everyNth("belohorodka", 2);
     }
 
     public static void everyNth(String text, int n) {
 
         int len = text.length();
+        int i = n;
 
-        while ( n <= len)
+        while ( i <= len)
 
         {
-            System.out.println( text.substring(n-1, n) );
-            n = n + n;
+            System.out.println( text.substring(i-1, i) );
+            i = i + n;
         }
 
 
